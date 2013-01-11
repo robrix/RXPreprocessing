a42a408230d88f410efde9a211bbd772f18ddb83
Rob Rix <rob.rix@me.com>
2013-01-11 14:48:50 -0500

    Documents RXLog and rx_fold in the readme.

--- RXFold.h
+++ RXFold.h
@@ -9,6 +9,11 @@
 `rx_fold(f, initial, ...)`
 
 Folds the varargs with the macro specified as the first argument. This macro must take two arguments: the first being the result thus far, and the second being the current element in the varargs. `initial` provides the value for the first invocation of `f` (when there are no results yet provided).
+
+Use it like this:
+
+	#define add(x, y) y + x
+	int sum = rx_fold(add, 0, 1, 2, 3, 4, 5); → int sum = 0 + 1 + 2 + 3 + 4 + 5 + 6;
 */
 
 #define rx_fold(f, initial, ...) \
